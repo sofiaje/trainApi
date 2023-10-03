948a62b3ffc235ccc8b0367118fb10e50ef69dad
SofiaJe <sofia.jespersen@hotmail.com>
2023-10-03 21:39:06 +0200

    cors

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(express.urlencoded({ extended: true }))
 // enable cors for all routes, change this
 app.use(
     cors({
-        origin: "http://127.0.0.1:5173",
+        origin: [
+            "http://127.0.0.1:5173",
+            "http://localhost:5173"
+        ]
 }));
 
 if (process.env.NODE_ENV !== "production") {
